a51c8fffe3a80f9e202d865dcb1d6262e2b75a58
Alina Yatsko <y.alina.1021@gmail.com>
2019-08-30 14:24:26 +0300

    Convert to USD

---
+++ convertor.py
@@ -0,0 +1,23 @@
+import requests
+
+api_data = requests.get('http://www.apilayer.net/api/live?access_key=2c68e2525a5851fb8a64fe0c5d5556de&format=1')
+data = api_data.json()
+# print("Json formatted: \n", data, "\n\n")
+
+
+def get_usd_uah():
+    try:
+        entered_num = float(input('Enter currency in UAN:\n'))
+    except ValueError:
+        entered_num = 0
+        print('Incorrect input')
+    rate = data['quotes']['USDUAH']
+    return entered_num, rate
+
+
+def exchange_to_usd(currency, rate):
+    print(str(currency / rate) + ' USD')
+
+
+curr, r = get_usd_uah()
+exchange_to_usd(curr, r)
